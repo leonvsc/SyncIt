10ddc1f11e85f2d11231dc2b45ef3ddf76c34608
Leon <leonvsc@users.noreply.github.com>
2024-04-02 21:13:30 +0200

    Fix: Removed variables

--- client/sync.go
+++ client/sync.go
@@ -2,9 +2,6 @@ package main
 
 import "net"
 
-var folderPath = "../local"
-var filePath = "../local/input.txt"
-
 func sync(conn net.Conn) {
 	//pushFolderToServer(conn, folderPath)
 	//pushFileToServer(conn, filePath)
